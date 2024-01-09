495a78cb3341a68f4a40edced64ec6717eb07b66
Trezlor <sondre-h@hotmail.com>
2024-01-09 04:30:58 +0100

    feat: make message dynamic if nickname or not

--- commands/utility/roll.js
+++ commands/utility/roll.js
@@ -3,9 +3,10 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder().setName('roll').setDescription('Roll a number between 0-100'),
 	async execute(interaction) {
+		const user = interaction.member;
 		await interaction.reply(
 			'```arm\n' +
-				interaction.user.displayName +
+				(!user.nickname ? user.displayName : user.nickname) +
 				' rolled ' +
 				Math.floor(Math.random() * 101) +
 				'!\n```'
