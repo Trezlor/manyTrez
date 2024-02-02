2fd69731086e189896bdb246e18be4ecb4530a41
Trezlor <sondre-h@hotmail.com>
2024-02-02 16:56:56 +0100

    feat: add autocomplete listener

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ client.on(Events.InteractionCreate, async (interaction) => {
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isAutocomplete()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.autocomplete(interaction);
+	} catch (error) {
+		return;
+	}
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
 	const command = interaction.client.commands.get(interaction.commandName);
 
